976ec845da60fa3ee3b3d4d00fb274bf45aa9dec
Sammuel Victor <samvss13@gmail.com>
2026-02-12 00:18:07 +0000

    2118

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      olá turma!
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-white">
+      Oi, tudo bem com você? espero que sim!
     </div>
   );
 }
