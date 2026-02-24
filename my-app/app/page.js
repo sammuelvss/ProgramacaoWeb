5e39434e96244f7033dbabf643f72fd7613cd6bb
Sammuel Victor <samvss13@gmail.com>
2026-02-24 00:25:49 +0000

    2125

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,9 +1,14 @@
-import Image from "next/image";
+import Profile from "@/components/Profile";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-white">
-      Oi, tudo bem com você? espero que sim!
+    <div className="flex min-h-screen flex-col items-center justify-center bg-zinc-50 font-sans dark:bg-white text-black">
+      <h1>Amazing scientists</h1>
+      <section className="flex gap-4">
+        <Profile />
+        <Profile />
+        <Profile />
+      </section>
     </div>
   );
 }
---
+++ my-app/components/Profile.js
@@ -0,0 +1,10 @@
+
+export default function Profile() {
+  return (
+    <img
+      src="https://i.imgur.com/rDYL4yg.jpeg"
+      alt="imagem aleatoria"
+      width={100}
+    />
+  )
+}
