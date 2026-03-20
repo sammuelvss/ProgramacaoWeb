b308983fdb423ad36734370871153a7e92efbca5
Sammuel Victor <samvss13@gmail.com>
2026-03-20 00:28:37 +0000

    2127

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,11 +1,11 @@
 
-import Contador from "@/components/GeradorAleatorio"
+import Balloon from "@/components/Balloon/Balloon"
 
 export default function Home (){
   return(
 
-    <div className="text-center"> 
-    <Contador />
+    <div className="text-center align-center bg-blue justify-center "> 
+    <Balloon />
      </div>
 
   )
--- my-app/components/Balloon/Balloon.js
+++ my-app/components/Balloon/Balloon.js
@@ -1,16 +1,70 @@
-const Balloon = () => {
+"use client";
+
+import { useState } from "react";
+
+
+export default function Balloon({ id = "1", color = "#9980FA" }) {
   const balloonWidth = 200;
-  const balloonHeight = 200 * 1.17;
+  const balloonHeight = balloonWidth * 1.17;
   const threadHeight = 50;
 
   return (
-    <div className="balloon">
+    <div className="balloon balloon--moving align-center" style={{ color: color }}>
       <svg
+        className="balloon-svg"
         xmlns="http://www.w3.org/2000/svg"
         viewBox={`0 0 ${balloonWidth} ${balloonHeight + threadHeight}`}
       >
-        ...
+        <defs>
+          <radialGradient
+            id={`balloon-gradient-${id}`}
+            cx="40%"
+            cy="40%"
+            r="50%"
+            fx="30%"
+            fy="30%"
+          >
+            <stop offset="0%" stopColor="#fff" />
+            <stop offset="100%" stopColor="currentColor" />
+          </radialGradient>
+          <filter
+            id={`balloon-shadow-${id}`}
+            x="0"
+            y="0"
+            width="100%"
+            height="100%"
+          >
+            <feMerge>
+              <feMergeNode in="offsetBlur" />
+              <feMergeNode in="SourceGraphic" />
+            </feMerge>
+          </filter>
+        </defs>
+
+        <rect
+          x={balloonWidth / 2}
+          y={balloonHeight}
+          width="1"
+          height={threadHeight}
+          fill="currentColor"
+        />
+        <polygon
+          points={`${balloonWidth / 2},${balloonHeight - 3} ${
+            balloonWidth / 2 + 8
+          },${balloonHeight + 5} ${balloonWidth / 2 - 8},${
+            balloonHeight + 5
+          }`}
+          fill="currentColor"
+        />
+        <ellipse
+          cx={balloonWidth / 2}
+          cy={balloonHeight / 2}
+          rx={balloonWidth / 2}
+          ry={balloonHeight / 2}
+          fill={`url(#balloon-gradient-${id})`}
+          filter={`url(#balloon-shadow-${id})`}
+        />
       </svg>
     </div>
   );
-};
+}
